eb3e2181b3d49bd5208dbea2226f95196220ee11
jeshika311 <jeshikatha@gmail.com>
2026-02-18 12:24:31 +0530

    BUILD: User Model

---
+++ Server/model/userModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['parent', 'driver', 'admin'],
+    default: 'parent',
+    required: true
+  },
+  verifyOtp: {
+    type: String,
+    default: ''
+  },
+  isAccountVerified: {
+    type: Boolean,
+    default: false
+  },
+  verifyOtpExpireAt: {
+    type: Number,
+    default: 0
+  },
+  resetOtp: {
+    type: String,
+    default: ''
+  },
+  resetOtpExpireAt: {
+    type: Number,
+    default: 0
+  }
+})
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default userModel;
