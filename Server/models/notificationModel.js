4c6d9e6de18b998344370bbaa4165e19891783fc
jeshika311 <jeshikatha@gmail.com>
2026-02-20 06:28:25 +0530

    BUILD: Notification Model

---
+++ Server/models/notificationModel.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  read: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true })
+
+const notificationModel = mongoose.models.notification || mongoose.model('notification', notificationSchema);
+
+export default notificationModel;
