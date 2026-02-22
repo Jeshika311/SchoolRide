ac087e19bb7ad2df93f8c909bc503e8aa974d83f
jeshika311 <jeshikatha@gmail.com>
2026-02-22 07:53:28 +0530

    BUILD: Location Model

--- Server/models/locationModel.js
+++ Server/models/locationModel.js
@@ -1,28 +1,40 @@
 import mongoose from 'mongoose';
 
 const locationSchema = new mongoose.Schema({
-  trip: {
+  trip_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'trip',
-    required: true
+    required: true,
+    index: true
   },
-  lat: {
-    type: Number,
-    required: true
-  },
-  lng: {
-    type: Number,
-    required: true
+  location : {
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point'
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      validate: {
+        validator: function(val){
+          return (
+            val.length === 2 &&
+            val[0] >= -180 && val[0] <= 180 &&
+            val[1] >= -90 && val[1] <= 90
+          );
+        },
+        message: 'Invalid longitude and latitude values'
+      }
+    }
   },
   speed: {
     type: Number,
     default: 0
-  },
-  timestamp: {
-    type: Date,
-    required: true
   }
 }, { timestamps: true })
+
+locationSchema.index({ location: '2dsphere' });
 
 const locationModel = mongoose.models.location || mongoose.model('location', locationSchema);
 
