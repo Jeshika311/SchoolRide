51c901a78ffe7fa72a66212b527b5c74a0e908db
jeshika311 <jeshikatha@gmail.com>
2026-02-22 07:55:09 +0530

    BUILD: Vehicle Model

--- Server/models/vehicleModel.js
+++ Server/models/vehicleModel.js
@@ -1,10 +1,17 @@
 import mongoose from 'mongoose';
 
 const vehicleSchema = new mongoose.Schema({
+  driver_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    index: true
+  },
   vehicle_number: { 
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    index: true
   },
   total_seats: {
     type: Number,
@@ -12,14 +19,17 @@ const vehicleSchema = new mongoose.Schema({
   },
   available_seats: {
     type: Number,
-    required: true
-  },
-  driver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true
+    required: true,
+    min: 0
   }
 }, { timestamps: true })
+
+vehicleSchema.pre('save', function (next) {
+  if (this.available_seats > this.total_seats) {
+    return next(new Error('Available seats cannot exceed total seats'));
+  }
+  next();
+});
 
 const vehicleModel = mongoose.models.vehicle || mongoose.model('vehicle', vehicleSchema);
 
