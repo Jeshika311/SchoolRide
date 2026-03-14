fd60a6d78c93c214537e2ba16fdfebd149380e28
jeshika311 <jeshikatha@gmail.com>
2026-03-14 07:52:50 +0530

    BUILD: Info Routes

--- Server/routes/infoRoutes.js
+++ Server/routes/infoRoutes.js
@@ -3,26 +3,7 @@ import { getPrivacy, getTerms } from '../controllers/infoController.js';
 
 const router = express.Router();
 
-/**
- * @swagger
- * /privacy:
- *   get:
- *     summary: Get privacy policy text
- *     responses:
- *       200:
- *         description: Privacy policy
- */
 router.get('/privacy', getPrivacy);
-
-/**
- * @swagger
- * /terms:
- *   get:
- *     summary: Get terms of service text
- *     responses:
- *       200:
- *         description: Terms of service
- */
 router.get('/terms', getTerms);
 
 export default router;
