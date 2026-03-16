2ed120c00e18f7bbb1890c683861f7d641584a83
jeshika311 <jeshikatha@gmail.com>
2026-03-16 12:02:19 +0530

    Remove Authorization while logout

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { register, login, verifyEmail, logout, sendResetOtp, googleLogin, sendVerifyOtp, verifyResetOtp, resetPassword } from '../controllers/authController.js';
-import AuthMiddleware from '../middlewares/AuthMiddleware.js';
 
 const authRouter = express.Router();
 
@@ -9,7 +8,7 @@ authRouter.post('/send-verify-otp', sendVerifyOtp);
 authRouter.post('/verify-email', verifyEmail);
 authRouter.post('/login', login);
 authRouter.post('/google-login', googleLogin); 
-authRouter.post('/logout', AuthMiddleware, logout);
+authRouter.post('/logout', logout);
 
 authRouter.post('/forgot-password', sendResetOtp);
 authRouter.post('/verify-reset-otp', verifyResetOtp);
