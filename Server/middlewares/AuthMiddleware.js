8dfd736f7fd2690e85d6d49e340396510278da36
jeshika311 <jeshikatha@gmail.com>
2026-03-16 12:19:18 +0530

    Update Logout Logic

--- Server/middlewares/AuthMiddleware.js
+++ Server/middlewares/AuthMiddleware.js
@@ -1,45 +1,47 @@
 import jwt from 'jsonwebtoken';
 
 const AuthMiddleware = async (req, res, next) => {
+  try {
 
-    try{
+    let token;
 
-      const {token} = req.cookies;
+    const authHeader = req.headers.authorization;
 
-      if(!token){
-        return res.status(401).json({
-          success:false, 
-          message: "Unauthorized: No token provided"
-        })
-      }
-
-      const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-
-      if(tokenDecode?.id){
-        req.user = {id: tokenDecode.id, role: tokenDecode.role};
-      }
-      else {
-        return res.status(401).json({
-          success: false,
-          message: "Unauthorized: Invalid token"
-        });
-      }
-
-      req.user = {
-        id: tokenDecode.id,
-        role: tokenDecode.role
-      };
-      next();
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
     }
 
-    catch(error){
-      console.log("JWT error:", error.message);
+    if (!token && req.cookies?.token) {
+      token = req.cookies.token;
+    }
+
+    if (!token) {
       return res.status(401).json({
         success: false,
-        message: 
-          error.message === "jwt expired" ? "Unauthorized: Token expired" : "Unauthorized: Invalid token"
+        message: "Unauthorized: No token provided"
       });
+    }
+
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = {
+      id: tokenDecode.id,
+      role: tokenDecode.role
+    };
+
+    next();
+
+  } catch (error) {
+    console.log("JWT error:", error.message);
+
+    return res.status(401).json({
+      success: false,
+      message:
+        error.message === "jwt expired"
+          ? "Unauthorized: Token expired"
+          : "Unauthorized: Invalid token"
+    });
   }
-}
+};
 
 export default AuthMiddleware;
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { register, login, verifyEmail, logout, sendResetOtp, googleLogin, sendVerifyOtp, verifyResetOtp, resetPassword } from '../controllers/authController.js';
+import AuthMiddleware from '../middlewares/AuthMiddleware.js';
 
 const authRouter = express.Router();
 
@@ -8,7 +9,7 @@ authRouter.post('/send-verify-otp', sendVerifyOtp);
 authRouter.post('/verify-email', verifyEmail);
 authRouter.post('/login', login);
 authRouter.post('/google-login', googleLogin); 
-authRouter.post('/logout', logout);
+authRouter.post('/logout', AuthMiddleware, logout);
 
 authRouter.post('/forgot-password', sendResetOtp);
 authRouter.post('/verify-reset-otp', verifyResetOtp);
